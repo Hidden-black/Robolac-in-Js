ed56ebd76a545863c0fb812ba0afe4051896d21d
HiddenBlack <68143131+Hidden-black@users.noreply.github.com>
2021-09-11 12:00:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+
+
+
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
